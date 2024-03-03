c075505b69023f82a24132f533d9ed513428f8a0
TruongKhanhLong <133574275+TruongKhanhLong@users.noreply.github.com>
2024-03-03 16:24:46 +0700

    Add files via upload

---
+++ src/hard/FirstMissingPositive.java
@@ -0,0 +1,29 @@
+package hard;
+
+public class FirstMissingPositive {
+    public static void main(String[] args) {
+        int[] nums = {-1,3,4,5,6};
+        System.out.println(firstMissingPositive(nums));
+    }
+
+    public static int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+                swap(nums, i, nums[i] - 1);
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        return n + 1;
+    }
+
+    public static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
